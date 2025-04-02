8e57890a51222905030ed7a710423cb6d1eeb25d
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-04-02 19:19:21 +0200

    create header file for Renderer class

---
+++ multidemo/Renderer.h
@@ -0,0 +1,36 @@
+// Copyright 2025 Giorgio Gamba
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
+
+namespace multidemo
+{
+
+	class Renderer
+	{
+	public:
+
+		Renderer(const int inWidth, const int inHeight);
+
+		~Renderer();
+
+		void run();
+
+		void render();
+
+	private:
+
+		int width;
+
+		int height;
+
+		SDL_Window* window;
+		SDL_Renderer* renderer;
+		SDL_Texture* texture;
+
+		// Array of integer containing the value for each pixel
+		int* frameBuffer;
+	};
+
+}
