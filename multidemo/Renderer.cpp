697511531420d9ea5039bab71c19b3ccf7a3c9b4
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-04-03 17:53:12 +0200

    add basic render loop for pixel drawing in window

--- multidemo/Renderer.cpp
+++ multidemo/Renderer.cpp
@@ -15,10 +15,24 @@ public:
 		window = SDL_CreateWindow("TEST", width, height, SDL_WINDOWPOS_CENTERED | SDL_WINDOWPOS_CENTERED);
 
 	};
+	void Renderer::render()
+	{
+		SDL_SetRenderScale(renderer, 4, 4);
 
-private:
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-	int width, height;
+		SDL_RenderClear(renderer);
 
-	SDL_Window* window;
-};
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				SDL_RenderPoint(renderer, i, j);
+			}
+		}
+
+		SDL_RenderPresent(renderer);
+	}
+}
