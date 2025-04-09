95d5ab69eb9a3a659be0be27204170ba41ceeb04
Giorgio Gamba <giorgiogamba97@gmail.com>
2025-04-09 16:37:04 +0200

    add pixel structure

---
+++ multidemo/Pixel.h
@@ -0,0 +1,36 @@
+// Copyright 2025 Giorgio Gamba
+
+#pragma once
+
+#include "SDL3/SDL_render.h"
+
+// This struct represents how a pixel is intended inside the texture drawn
+struct Pixel
+{
+
+public:
+
+	Pixel(const int inR, const int inG, const int inB)
+		: r(inR)
+		, g(inG)
+		, b(inB)
+	{
+		const Uint8 red = static_cast<Uint8>(r);
+		const Uint8 green = static_cast<Uint8>(g);
+		const Uint8 blue = static_cast<Uint8>(b);
+
+		constexpr int a = 255;
+		color = (a << 24) | (red << 16) | (green << 8) | blue;
+	}
+
+	Uint32 getColor() const { return color; }
+
+private:
+
+	Uint32 color;
+
+	// The three quantities representing the pixel color
+	int r, g, b;
+
+	// Alpha is assumed to always be at max value (255 in 8 bits)
+};
